e25786b1e5086a851e474cb373d9641d8658d963
LemonLzy <lzy291980138@163.com>
2023-06-06 21:07:15 +0800

    Add：初始化工具前端

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,34 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <el-tabs v-model="activeName" class="openTable">
+      <el-tab-pane label="个人开户" name="individual">
+        <IndividualOpen/>
+      </el-tab-pane>
+    </el-tabs>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script lang="ts" setup>
+import IndividualOpen from '@/components/Individual.vue';
+import {ref} from 'vue'
+
+const activeName = ref('individual')
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+#app {
+  padding: 0;
+  margin-left: 20px;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid rgba(134, 140, 151, 0.24);
+  border-radius: 3px;
+  max-width: 1000px;
+  box-shadow: rgb(53 64 82 / 4%) 0 2px 4px 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.openTable {
+  padding-left: 20px;
+  padding-top: 10px;
 }
 </style>
---
+++ web/src/components/Individual.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>testestet</h1>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
